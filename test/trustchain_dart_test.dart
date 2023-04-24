72842f51294d8e841ff7d4bde01b4bc2c7ef9741
Ed Chapman - Turing <edchapmanelc@gmail.com>
2023-04-24 15:17:28 +0100

    credential model

--- test/trustchain_dart_test.dart
+++ test/trustchain_dart_test.dart
@@ -1,4 +1,6 @@
+
 import 'package:trustchain_dart/trustchain_dart.dart';
+import 'package:trustchain_dart/src/credentials/credential.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -11,6 +13,41 @@ void main() {
 
     test('First Test', () {
       expect(awesome.isAwesome, isTrue);
+    });
+  });
+
+  group('CredentialModel', () {
+    test('.toMap() encodes to map', () {
+      final credential = CredentialModel(
+          id: 'uuid', alias: null, image: 'image', data: {'issuer': 'did:...'});
+      final m = credential.toMap();
+
+      expect(
+          m,
+          equals({
+            'id': 'uuid',
+            'alias': null,
+            'image': 'image',
+            'data': {'issuer': 'did:...'}
+          }));
+    });
+
+    test('.fromMap() with only data field should generate an id', () {
+      final m = {
+        'data': {'issuer': 'did:...'}
+      };
+      final credential = CredentialModel.fromMap(m);
+      expect(credential.id, isNotEmpty);
+      expect(credential.data, equals(m['data']));
+    });
+
+    test('.fromMap() with id should not generate a new id', () {
+      final m = {
+        'id': 'uuid',
+        'data': {'issuer': 'did:...'}
+      };
+      final credential = CredentialModel.fromMap(m);
+      expect(credential.id, equals('uuid'));
     });
   });
 }
