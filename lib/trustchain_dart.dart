1060aeed3cbdc9b1c57950277374e7a25d427e79
Ed Chapman - Turing <edchapmanelc@gmail.com>
2023-06-01 13:33:07 +0100

    expose file utilites

--- lib/trustchain_dart.dart
+++ lib/trustchain_dart.dart
@@ -9,4 +9,5 @@ export 'src/chain/chain.dart';
 export 'src/did/did.dart';
 export 'src/credentials/credential.dart';
 export 'src/utils/errorhandling.dart';
+export 'src/utils/device_storage.dart';
 // TODO: Export any libraries intended for clients of this package.
