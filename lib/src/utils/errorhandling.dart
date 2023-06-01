9728c9c2d08cddbd609def9d9dd38f6479d9cddc
Ed Chapman - Turing <edchapmanelc@gmail.com>
2023-06-01 09:52:42 +0100

    error handling enum

---
+++ lib/src/utils/errorhandling.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_rust_bridge/flutter_rust_bridge.dart';
+
+class ParseFfiErrorResult {
+  final FfiError varient;
+  final String info;
+  ParseFfiErrorResult(this.varient,this.info);
+}
+
+enum FfiError {
+  failedToDeserialise("JSON Deserialisation Error"),
+  failedToCreateDID("DID Create Error"),
+  failedToAttestdDID("dDID Attest Error"),
+  failedToResolveDID("DID Resolve Error"),
+  failedToVerifyDID("DID Verify Error"),
+
+  unrecognisedFfiExceptionCode("No FFiError enum varient implimented for this exception code");
+
+  const FfiError(this.code);
+  final String code;
+
+  static ParseFfiErrorResult parseFfiError(FfiException err) {
+    var parts = err.message.split(":");
+    final code = parts.removeAt(0);
+    final info = parts.join(":");
+    for (final errVarient in FfiError.values) {
+      if (code == errVarient.code) {
+        return ParseFfiErrorResult(errVarient, info);
+      }
+    }
+    return ParseFfiErrorResult(FfiError.unrecognisedFfiExceptionCode, info);
+  }
+  
+}
+
