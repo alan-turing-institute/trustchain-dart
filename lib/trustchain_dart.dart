8dad306a5366c77d398b8c9c0ef09afe0c18784c
Ed Chapman - Turing <edchapmanelc@gmail.com>
2023-06-01 09:54:53 +0100

    expose error enum over public api

--- lib/trustchain_dart.dart
+++ lib/trustchain_dart.dart
@@ -8,4 +8,5 @@ export 'src/trustchain_dart_base.dart';
 export 'src/chain/chain.dart';
 export 'src/did/did.dart';
 export 'src/credentials/credential.dart';
+export 'src/utils/errorhandling.dart';
 // TODO: Export any libraries intended for clients of this package.
